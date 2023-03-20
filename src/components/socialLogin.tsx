87a352eb692cc94588cb5dcb874bab85d6748901
Hyeong-Joon <gudwns1020@naver.com>
2023-03-20 21:21:40 +0900

    #20.5 Github Log in

--- src/components/socialLogin.tsx
+++ src/components/socialLogin.tsx
@@ -17,7 +17,12 @@ export default function SocialLogin() {
         <Divider />
       </HStack>
       <VStack>
-        <Button w="100%" leftIcon={<FaGithub />} colorScheme={"linkedin"}>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=54c145a4cd5b954d7bc7&scope=read:user,user:email"
+          w="100%"
+          leftIcon={<FaGithub />}
+        >
           Continue with Github
         </Button>
         <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>

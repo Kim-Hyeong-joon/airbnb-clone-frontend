1d6e3b7f774580d20f45c04e13a8dcdd7aa4c515
Hyeong-Joon <gudwns1020@naver.com>
2023-04-05 12:00:06 +0900

    https://airbnb-frontend-syyh.onrender.com/

--- src/api.ts
+++ src/api.ts
@@ -10,7 +10,10 @@ import { IBooking } from "./routes/RoomReservation";
 import { IRoomDetail } from "./types";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://127.0.0.1:8000/api/v1/"
+      : "https://airbnbclone-9g3l.onrender.com/api/v1/",
   withCredentials: true,
 });
 
